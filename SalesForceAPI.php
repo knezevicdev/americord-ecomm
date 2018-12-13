33383bf7ba2814fe89bbc635bdf553b9571b5da7
Nikola Knežević <knezevicdev@gmail.com>
2018-12-13 09:55:48 +0100

    Objects update, opportunity search and payment

--- SalesForceAPI.php
+++ SalesForceAPI.php
@@ -4,6 +4,7 @@ namespace App;
 
 use GuzzleHttp\Client;
 use Exception;
+use GuzzleHttp\Exception\ClientException;
 
 class SalesForceAPI {
 
@@ -13,6 +14,29 @@ class SalesForceAPI {
         'client_secret',
         'username',
         'password'
+    ];
+
+    private const OPPORTUNITY_FIELDS = [
+        'Ecomm_Status__c',
+        'Baby_s_Due_Date__c',
+        'FirstName__c',
+        'LastName__c',
+        'Email__c',
+        'Phone__c',
+        'Relation_to_Baby__c',
+        'Contact2_FirstName__c',
+        'Contact2_LastName__c',
+        'Contact2_Email__c',
+        'Contact2_Phone__c',
+        'Contact2_Relation_to_Baby__c',
+        'Surrogate__c',
+        'Coupon_Code__c',
+        'Sales_Discount__c',
+        'Referred_by_email_address__c',
+        'Number_of_Births__c',
+        'Product2',
+        'Payment_Plans__c',
+        'Storage_Plan__c'
     ];
 
     private $api_url = null;
@@ -41,12 +65,10 @@ class SalesForceAPI {
 
         $this->client = new Client([
             'base_uri' => $this->api_url,
-            'timeout'  => 5.0,
+            'timeout'  => 30.0,
         ]);
 
         $response = $this->get_oauth_token();
-
-        var_dump($this->access_token);
     }
 
     private function get_oauth_token()
@@ -70,5 +92,97 @@ class SalesForceAPI {
         $this->access_token = $response->access_token;
     }
 
+    public function create_record($data, $name)
+    {
+        try {
+            $response = $this->client->request('POST', '/services/data/v44.0/sobjects/'.$name.'/', [
+                'json' => $data,
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->access_token
+                ]
+            ]);
 
+            $response = json_decode($response->getBody()->getContents());
+
+            return $response;
+        } catch(ClientException $exception) {
+            var_dump($exception->getMessage());
+            return false;
+        }
+    }
+
+    public function update_record($id, $data, $name)
+    {
+        try {
+            $response = $this->client->request('PATCH', '/services/data/v44.0/sobjects/'.$name.'/'.$id, [
+                'json' => $data,
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->access_token
+                ]
+            ]);
+        } catch(ClientException $exception) {
+            var_dump($exception->getMessage());
+        }
+    }
+
+    public function find_opportunity($email, $phone, $email2 = false, $phone2 = false)
+    {
+        $query = "SELECT id FROM Opportunity WHERE Email__c = '{$email}' OR Contact2_Email__c = '{$email}' OR Phone__c = '{$phone}' OR Contact2_Phone__c = '{$phone}'";
+
+        if($email2 && $email2 !== NULL) $query .= " OR Email__c = '{$email2}' OR Contact2_Email__c = '{$email2}'";
+        if($phone2 && $phone2 !== NULL) $query .= " OR Phone__c = '{$phone2}' OR Contact2_Phone__c = '{$phone2}'";
+        
+        try {
+            $response = $this->client->request('GET', '/services/data/v44.0/query/', [
+                'query' => [
+                    'q' => $query
+                ],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->access_token
+                ]
+            ]);
+            
+            if($response->getStatusCode() === 200) {
+                $response = json_decode($response->getBody()->getContents());
+
+                if($response->done && count($response->records) > 0) {
+                    return $response->records[0];
+                }
+            }
+        } catch(ClientException $exception) {
+            var_dump($exception->getMessage());
+        }
+        return false;
+    }
+
+    public function pay($ccNumber, $cvv, $expMonth, $expYear, $firstName, $lastName, $zip, $object, $id)
+    {
+        try {
+            $response = $this->client->request('POST', '/services/apexrest/fw1/v2/payments', [
+                'json' => [
+                    "amount"                => 199.00, 
+                    "credit_card_number"    => $ccNumber, 
+                    "cvv2"                  => $cvv,
+                    "expiry_month"          => $expMonth, 
+                    "expiry_year"           => $expYear, 
+                    "first_name"            => $firstName, 
+                    "last_name"             => $lastName, 
+                    "billing_zip"           => $zip,
+                    "Related_object_field"  => $object,
+                    "Related_record_id"     => $id,
+                    "Reference"             => "ecomm"
+                ],
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->access_token
+                ]
+            ]);
+
+            $response = json_decode($response->getBody()->getContents());
+            
+            return $response;
+        } catch(ClientException $exception) {
+            var_dump($exception->getMessage());
+        }
+        return false;
+    }
 }
--- index.php
+++ index.php
@@ -17,3 +17,45 @@ $sfApi = new SalesForceAPI([
     'username'      => getenv('SF_USERNAME'),
     'password'      => getenv('SF_PASSWORD')
 ]);
+
+// $sfApi->create_opportinity([
+//     'Baby_s_Due_Date__c' => '2019-28-04',
+//     'FirstName__c' => 'Nikola',
+//     'LastName__c' => 'Knezevic',
+//     'Email__c' => 'knezevicdev@gmail.com',
+//     'Phone__c' => '(888) 987-2345',
+//     'Relation_to_Baby__c' => 'Mother',
+//     'Number_of_Births__c' => 'Single',
+//     'Surrogate__c' => 'false',
+//     'Payment_Plans__c' => 'Annual Storage',
+//     'Storage_Plan__c' => 'Annual Storage Plan',
+//     'Name' => 'test16 test16 - 11/30/2018',
+//     'StageName' => 'Started',
+//     'CloseDate' => '2019-28-04',
+//     'Cord_Blood_2_0__c' => 1
+// ]);
+
+// $sfApi->create_record([
+//     'Babys_Due_Date__c' => '2019-28-04',
+//     'FirstName' => 'Nikola',
+//     'LastName' => 'Knezevic',
+//     'Email' => 'knezevicdev@gmail.com',
+//     'Phone' => '(888) 987-2345',
+//     'Relation_to_Baby__c' => 'Mother',
+//     'Number_of_Babies__c' => 'Single',
+//     'Surrogate__c' => 'false',
+//     'Payment_Plans__c' => 'Annual Storage',
+//     'Storage_Plan__c' => 'Annual Storage Plan',
+//     'Cord_Blood_2_0__c' => 1
+// ], 'Lead');
+
+// $sfApi->update_record('0065C000003yx7u', [
+//     'Cord_Tissue__c' => NULL
+// ], 'Opportunity');
+
+// $opportunity = $sfApi->find_opportunity("aaa", "test");
+
+//fw1__Opportunity__c
+//Lead__c
+
+$sfApi->pay("4111111111111111", "321", "12", "2019", "Nikola", "Knezevic", "22222", "fw1__Opportunity__c", "0065C000003yx7u");
